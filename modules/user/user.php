d7df2b79fdd5b3070e38859d0d6592d40628995e
Xedecimal <Xedecimal@gmail.com>
2011-09-02 19:08:10 -0700

    User will adhere to extended module names now without having to specifically designate.

--- modules/user/user.php
+++ modules/user/user.php
@@ -10,7 +10,7 @@ class ModUser extends Module
 {
 	/**
 	 * @var LoginManager Associated login manager.
-	 * 
+	 *
 	 */
 	public $Block = 'user';
 
@@ -32,10 +32,12 @@ class ModUser extends Module
 		)
 	);
 
+	public $Name = 'user';
+
 	function __construct()
 	{
 		$this->Behavior = new ModUserBehavior();
-		$this->CheckActive('user');
+		$this->CheckActive($this->Name);
 
 		global $_d;
 		$_d['user.session.user'] = 'user_sessuser';
