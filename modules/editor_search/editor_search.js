db470ff86ac432bc87f1db7b637ce6e5ea654ca5
Nicholas Libby <Xedecimal@gmail.com>
2012-08-08 12:49:12 -0700

    Fixed up the pagination and classed values in results to be easier to pretty up with css.

--- modules/editor_search/editor_search.js
+++ modules/editor_search/editor_search.js
@@ -8,30 +8,38 @@ $(function () {
 		var targ = $(this).attr('id').match(/hider_(.*)/)[1];
 		if (!$(this).attr('checked')) $('#hidden_'+targ).hide();
 		$('#hidden_'+targ.replace('.', '\\.')).showHide($(this).attr('checked'));
-		$(this).click(function () {
+		$(this).on('click', function (e) {
 			var obj = $('#hidden_'+targ.replace('.', '\\.'));
 			$('#hidden_'+targ.replace('.', '\\.')).showHide($(this).attr('checked'));
 		});
 	});
 
-	$('.delResult').live('click', function () {
+	$(document).on('click', '.delResult', function (e) {
+		e.preventDefault();
 		if (confirm('Are you sure you wish to delete this entry?'))
 		{
 			var id = $(this).attr('href').match(/(\d+)$/)[1];
 			$.post($(this).attr('href'), null, function (data) {
 				$('#result-'+id).hide();
+				alert('Done.');
 			}, 'json');
+			e.preventDefault();
 		}
-		return false;
 	});
 
 	$('.actions a,.action').button();
 
-	$('#but-search').click(function () {
+	$(document).on('click', '#but-search', function (e) {
+		e.preventDefault();
 		$form = $('#data_search_'+$(this).data('name'));
 		$.post($form.attr('action'), $form.serialize(), function (data) {
 			$('#div-results').html(data);
 			$('.actions a,.action').button();
 		});
+	});
+	
+	$(document).on('click', 'a.a-page', function (e) {
+		e.preventDefault();
+		$('#div-results').load($(this).attr('href'));
 	});
 });
