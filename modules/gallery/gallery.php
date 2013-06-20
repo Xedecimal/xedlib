3a5b5345f19aeeb5e1ac9f6552619c4c21b6aa5f
Nicholas Libby <Xedecimal@gmail.com>
2013-06-20 10:36:14 -0700

    Added a Gallery->Title to give it a name to present on the navigation.

--- modules/gallery/gallery.php
+++ modules/gallery/gallery.php
@@ -320,6 +320,7 @@ class GalleryAdmin extends FileManager
 {
 	public $Name = 'admin/gallery';
 	public $Root = 'galimg';
+	public $Title = 'Gallery';
 
 	function Auth() { return User::RequireAccess(1); }
 
@@ -338,7 +339,7 @@ class GalleryAdmin extends FileManager
 	{
 		global $_d, $me;
 
-		$_d['nav.links']['Admin/Gallery'] = '{{app_abs}}/admin/gallery';
+		$_d['nav.links']['Admin/'.$this->Title] = '{{app_abs}}/'.$this->Name;
 	}
 
 	function Prepare()
